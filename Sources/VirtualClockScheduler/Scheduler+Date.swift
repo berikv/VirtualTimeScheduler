3ac356627875d6219adf965188a877651ce0d763
Berik Visschers <berik@visschers.nu>
2022-01-17 19:25:03 +0100

    Partial implementation

---
+++ Sources/VirtualClockScheduler/Scheduler+Date.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+import Combine
+
+extension Scheduler {
+    var date: Date {
+        Date()
+    }
+}
--- Tests/VirtualClockSchedulerTests/VirtualClockSchedulerTests.swift
+++ Tests/VirtualClockSchedulerTests/VirtualClockSchedulerTests.swift
@@ -6,6 +6,5 @@ final class VirtualClockSchedulerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(VirtualClockScheduler().text, "Hello, World!")
     }
 }
